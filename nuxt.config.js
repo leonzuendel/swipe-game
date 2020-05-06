72d254683ace50fe3781eb5c668bcce8ab8841d2
Leon Zündel <leonzuendel1@gmail.com>
2020-05-07 01:23:44 +0200

    master update

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,13 +8,32 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      {
+        name: 'apple-mobile-web-app-status-bar-style',
+        content: 'black-translucent'
+      },
+
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;500;700&display=swap'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
